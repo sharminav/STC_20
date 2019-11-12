55cb4a2eb3392ea86730031d3ebd8321df788b86
sahrminav <baclozyabr@rambler.ru>
2019-11-12 17:32:01 +0300

    hometask 5: сортировка по имени хозяина, имени животного и весу животного

--- lessons/lesson_05/ComparatorAnimals.java
+++ lessons/lesson_05/ComparatorAnimals.java
@@ -5,10 +5,9 @@ import java.util.Comparator;
 public class ComparatorAnimals implements Comparator<Animal> {
 
     public int compare(Animal o1, Animal o2) {
-        //System.out.println(o1.getName().toString() + o2.getName().toString() + o1.getName().compareTo(o2.getName()));
-        if (o1.getName().compareTo(o2.getName()) < 0
-                || o1.getName().compareTo(o2.getName()) <= 0 && o1.getWeight() < o2.getWeight()
-                || o1.getName().compareTo(o2.getName()) == 0 && o1.getWeight() < o2.getWeight()) {
+        if (o1.getOwnerName().compareTo(o2.getOwnerName()) < 0
+                || o1.getOwnerName().compareTo(o2.getOwnerName()) <= 0 && o1.getName().compareTo(o2.getName()) < 0
+                || o1.getOwnerName().compareTo(o2.getOwnerName()) <= 0 && o1.getName().compareTo(o2.getName()) <= 0 && o1.getWeight() < o2.getWeight()) {
                 return -1;
         }
         else {
